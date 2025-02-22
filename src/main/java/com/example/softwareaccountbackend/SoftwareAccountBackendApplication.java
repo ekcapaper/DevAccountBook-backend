bbe38f88469bbfed582fa0d63e4828be633f8040
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-02-22 16:37:27 +0900

    Init Commit

---
+++ src/main/java/com/example/softwareaccountbackend/SoftwareAccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareaccountbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAccountBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareAccountBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwareaccountbackend/SoftwareAccountBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwareaccountbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareAccountBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
