23a84f22f5dff48de5f6ac4a562821bfbd061268
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-03-01 14:59:13 +0900

    refactor : API 수정

--- src/main/java/com/ekcapaper/software/accounting/backend/controller/TechnicalContextDecisionController.java
+++ src/main/java/com/ekcapaper/software/accounting/backend/controller/TechnicalContextDecisionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/technical/relationships")
+@RequestMapping("/api/technical/associations")
 @RequiredArgsConstructor
 public class TechnicalContextDecisionController {
 
